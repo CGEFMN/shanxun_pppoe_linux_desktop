9d6c0bfaa8219b811ff10387ab3bfd07f47c0fb0
MNEF <cgsigmax@gmail.com>
2018-10-18 18:29:17 +0800

    Add iproute2 support
    原先的route属于net-tools套件，该套件许多年前就已经没有维护了，已被许多linux发行版废弃使用。
    iproute2套件里提供了许多增强功能的命令，ip命令即是其中之一。route合并到了其中。

--- pppd_sx.sh
+++ pppd_sx.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-route del default
+if hash ip 2>/dev/null; then
+    ip route delete default
+else
+    route delete default
+fi
 pppd nodetach defaultroute usepeerdns maxfail 1 user $1 password $2 mtu 1492 mru 1492 plugin rp-pppoe.so nic-eth0 plugin zhejiang_xiaoyuan_sxplugin.so
